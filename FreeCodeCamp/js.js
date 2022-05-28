4197064613e3628e16938e0af9514dcbb703f179
beniv_bohdan <bohdanbeniv@gmail.com>
2022-05-28 17:04:18 +0200

    Global vs. Local Scope in Functions

--- FreeCodeCamp/js.js
+++ FreeCodeCamp/js.js
@@ -342,3 +342,18 @@ function myLocalScope() {
   // Run and check the console
   // myVar is not defined outside of myLocalScope
   console.log('outside myLocalScope', myVar);
+
+//   Global vs. Local Scope in Functions
+// It is possible to have both local and global variables with the same name. When you do this, the local variable takes precedence over the global variable.
+
+// In this example:
+
+// const someVar = "Hat";
+
+// function myFun() {
+//   const someVar = "Head";
+//   return someVar;
+// }
+// The function myFun will return the string Head because the local version of the variable is present.
+
+// Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
