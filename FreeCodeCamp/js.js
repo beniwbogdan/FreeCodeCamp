452a79b3f66da5a239200dda031e8777e44e1871
beniv_bohdan <bohdanbeniv@gmail.com>
2022-05-28 15:01:37 +0200

    array in array

--- FreeCodeCamp/js.js
+++ FreeCodeCamp/js.js
@@ -54,3 +54,15 @@ const wordBlanks =myAdjective+" "+myNoun+" "+myVerb+" "+myAdverb ; // Change thi
 
 // Only change code below this line
 const myArray = ["string",4];
+
+
+
+// Nest one Array within Another Array
+// You can also nest arrays within other arrays, like below:
+
+// const teams = [["Bulls", 23], ["White Sox", 45]];
+// This is also called a multi-dimensional array.
+
+// Create a nested array called myArray.
+// Only change code below this line
+const myArray = [[],[]];
