e3d1122f0c5621c988e5d0f5103d79f1961495e0
beniv_bohdan <bohdanbeniv@gmail.com>
2022-05-28 17:12:43 +0200

    Understanding Undefined Value returned from a Function

--- FreeCodeCamp/js.js
+++ FreeCodeCamp/js.js
@@ -357,3 +357,48 @@ function myLocalScope() {
 // The function myFun will return the string Head because the local version of the variable is present.
 
 // Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+const outerWear="sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+// Understanding Undefined Value returned from a Function
+// A function can include the return statement but it does not have to. In the case that the function doesn't have a return statement, when you call it, the function processes the inner code but the returned value is undefined.
+
+// Example
+
+// let sum = 0;
+
+// function addSum(num) {
+//   sum = sum + num;
+// }
+
+// addSum(3);
+// addSum is a function without a return statement. The function will change the global sum variable but the returned value of the function is undefined.
+
+// Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+let addFive=()=>{
+sum+=5;
+
+} 
+
+// Only change code above this line
+
+addThree();
+addFive();
