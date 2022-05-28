41577d9e878f976e3fce47cb9680e76e02bf1070
beniv_bohdan <bohdanbeniv@gmail.com>
2022-05-28 15:05:00 +0200

    Modify Array Data With Indexes

--- FreeCodeCamp/js.js
+++ FreeCodeCamp/js.js
@@ -81,3 +81,26 @@ const myArray = [[],[]];
 // The console.log(array[0]) prints 50, and data has the value 60.
 
 // Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+
+const myArray = [50, 60, 70];
+
+const myData = myArray[0];
+
+
+// Modify Array Data With Indexes
+// Unlike strings, the entries of arrays are mutable and can be changed freely, even if the array was declared with const.
+
+// Example
+
+// const ourArray = [50, 40, 30];
+// ourArray[0] = 15;
+// ourArray now has the value [15, 40, 30].
+
+// Note: There shouldn't be any spaces between the array name and the square brackets, like array [0]. Although JavaScript is able to process this correctly, this may confuse other programmers reading your code.
+
+// Modify the data stored at index 0 of myArray to a value of 45.
+
+// Setup
+const myArray = [18, 64, 99];
+myArray[0]=45;
+// Only change code below this line
