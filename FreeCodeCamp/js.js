ffd83bb17a2e15498222995bcddde6c19b0954e6
beniv_bohdan <bohdanbeniv@gmail.com>
2022-05-29 12:43:06 +0200

    Return Early Pattern for Functions

--- FreeCodeCamp/js.js
+++ FreeCodeCamp/js.js
@@ -1233,3 +1233,29 @@ function chainToSwitch(val) {
 // }
 // Fix the function isLess to remove the if/else statements.
 
+function isLess(a, b) {
+    // Only change code below this line
+    return a < b;
+    
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
+
+//   Return Early Pattern for Functions
+// When a return statement is reached, the execution of the current function stops and control returns to the calling location.
+
+// Example
+
+// function myFun() {
+//   console.log("Hello");
+//   return "World";
+//   console.log("byebye")
+// }
+// myFun();
+// The above will display the string Hello in the console, and return the string World. The string byebye will never display in the console, because the function exits at the return statement.
+
+// Modify the function abTest so that if a or b are less than 0 the function will immediately exit with a value of undefined.
+
+// Hint
+// Remember that undefined is a keyword, not a string.
