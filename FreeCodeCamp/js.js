ea908f5a50bf6150090e07c0a9b282b97e5f6b16
beniv_bohdan <bohdanbeniv@gmail.com>
2022-05-29 12:33:01 +0200

    Returning Boolean Values from Functions

--- FreeCodeCamp/js.js
+++ FreeCodeCamp/js.js
@@ -1160,3 +1160,76 @@ function sequentialSizes(val) {
   }
   
   sequentialSizes(1);
+//   Replacing If Else Chains with Switch
+//   If you have many options to choose from, a switch statement can be easier to write than many chained if/else if statements. The following:
+  
+//   if (val === 1) {
+//     answer = "a";
+//   } else if (val === 2) {
+//     answer = "b";
+//   } else {
+//     answer = "c";
+//   }
+//   can be replaced with:
+  
+//   switch (val) {
+//     case 1:
+//       answer = "a";
+//       break;
+//     case 2:
+//       answer = "b";
+//       break;
+//     default:
+//       answer = "c";
+//   }
+//   Change the chained if/else if statements into a switch statement.
+
+function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  
+  
+  switch(val){
+  case "bob":
+      answer = "Marley";
+      break;
+  case 42:
+      answer = "The Answer";
+      break;
+  case 1:
+     answer = "There is no #1";
+      break;
+  case 99:
+      answer = "Missed me by this much!";
+      break;
+  case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  chainToSwitch(7);
+
+//   Returning Boolean Values from Functions
+// You may recall from Comparison with the Equality Operator that all comparison operators return a boolean true or false value.
+
+// Sometimes people use an if/else statement to do a comparison, like this:
+
+// function isEqual(a, b) {
+//   if (a === b) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// But there's a better way to do this. Since === returns true or false, we can return the result of the comparison:
+
+// function isEqual(a, b) {
+//   return a === b;
+// }
+// Fix the function isLess to remove the if/else statements.
+
