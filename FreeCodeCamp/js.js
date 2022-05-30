3a53bcffe65a943854712ccaccd6c9c472522295
beniv_bohdan <bohdanbeniv@gmail.com>
2022-05-30 19:05:04 +0200

    Build JavaScript Objects

--- FreeCodeCamp/js.js
+++ FreeCodeCamp/js.js
@@ -1259,3 +1259,55 @@ function isLess(a, b) {
 
 // Hint
 // Remember that undefined is a keyword, not a string.
+
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+  
+  if(a<0 || b<0){
+    return undefined;
+  }
+  
+    // Only change code above this line
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+  abTest(2,2);
+
+//   Build JavaScript Objects
+// You may have heard the term object before.
+
+// Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
+
+// Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.
+
+// Here's a sample cat object:
+
+// const cat = {
+//   "name": "Whiskers",
+//   "legs": 4,
+//   "tails": 1,
+//   "enemies": ["Water", "Dogs"]
+// };
+// In this example, all the properties are stored as strings, such as name, legs, and tails. However, you can also use numbers as properties. You can even omit the quotes for single-word string properties, as follows:
+
+// const anotherObject = {
+//   make: "Ford",
+//   5: "five",
+//   "model": "focus"
+// };
+// However, if your object has any non-string properties, JavaScript will automatically typecast them as strings.
+
+// Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
+
+// You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array.
+const myDog = {
+    // Only change code below this line
+  
+  name:"",
+  legs:4,
+  tails:1,
+  friends:["Jesse Pinkman", "Walter White"],
+    // Only change code above this line
+  };
